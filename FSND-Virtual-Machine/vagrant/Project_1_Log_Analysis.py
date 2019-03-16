a1f09b348492648b0508740bee26ac951b5bda8a
Jose Medina <joseakamaru@gmail.com>
2019-03-16 17:23:43 -0500

    small revisions on syntax and apparance

--- FSND-Virtual-Machine/vagrant/Project_1_Log_Analysis.py
+++ FSND-Virtual-Machine/vagrant/Project_1_Log_Analysis.py
@@ -16,7 +16,7 @@ query_0_8 = "select date(time) as date, count(*) as total from log group by date
 #List of answer queries
 query_1 = "select title, views from views_table order by views desc limit 3;"
 query_2 = "select name, sum(views) as views from views_table, authors where views_table.author = authors.id group by author, authors.name order by views desc;"
-query_3 = "select to_char(error_table.date, 'fmmonth dd, yyyy'), (error::real/total*100)::decimal(4, 2) from error_table, total_table where error_table.date = total_table.date and (error::float/total) >= 0.01;"
+query_3 = "select to_char(error_table.date, 'FMMonth dd, yyyy'), (error::real/total*100)::decimal(4, 2) from error_table, total_table where error_table.date = total_table.date and (error::float/total) >= 0.01;"
 
 
 #Create view tables:
@@ -59,21 +59,25 @@ def print_results(question_text, query_answer, end_text):
 #been accessed the most? Present this information as a sorted list with the
 #most popular article at the top.
 
-question_1 = "What are the most popular three articles of all time?"
+question_1 = "1 - What are the most popular three articles of all time?"
 
 #2. Who are the most popular article authors of all time? That is, when you sum
 #up all of the articles each author has written, which authors get the most page
 #views? Present this as a sorted list with the most popular author at the top.
 
-question_2 = "Who are the most popular article authors of all time?"
+question_2 = "2 - Who are the most popular article authors of all time?"
 
 #3. On which days did more than 1% of requests lead to errors? The log table
 #includes a column status that indicates the HTTP status code that the news
 #site sent to the user's browser.
 
-question_3 = "On which days did more than 1% of requests lead to errors?"
+question_3 = "3 - On which days did more than 1% of requests lead to errors?"
 
 if __name__ == "__main__":
+    print('-' * 60)
+    print('-' * 60)
+    print('\n' + ' '*20 + 'News Log Report')
+
     #create view tables
     view_table_function(view_1)
     view_table_function(view_2)
@@ -93,3 +97,7 @@ if __name__ == "__main__":
     view_table_function(delete_view_1)
     view_table_function(delete_view_2)
     view_table_function(delete_view_3)
+
+    print('\n' + ' '*26 + 'End')
+    print('-' * 60)
+    print('-' * 60)
