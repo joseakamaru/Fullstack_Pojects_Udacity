c08207a4770e8d1f2ca823dde391fc0cf6e25da5
Jose Medina <joseakamaru@gmail.com>
2019-03-16 14:08:38 -0500

    improve print function

--- FSND-Virtual-Machine/vagrant/Project_1_Log_Analysis.py
+++ FSND-Virtual-Machine/vagrant/Project_1_Log_Analysis.py
@@ -9,6 +9,8 @@ DBNAME = "news"
 #List of queries:
 query_1 = "select author from articles;"
 query_2 = "select * from authors;"
+query_3 = "select title, name from articles join authors on articles.author = authors.id;"
+
 def execute_posts(query):
     """Return all posts from the 'database', most recent first."""
     db = psycopg2.connect(database=DBNAME)
@@ -17,6 +19,10 @@ def execute_posts(query):
     posts = c.fetchall()
     db.close()
     return posts
+
+def print_results(object):
+    for obj in object:
+        print(obj)
 
 #2. Who are the most popular article authors of all time? That is, when you sum
 #up all of the articles each author has written, which authors get the most page
@@ -27,4 +33,5 @@ def execute_posts(query):
 #site sent to the user's browser.
 
 if __name__ == "__main__":
-    print(execute_posts(query_2))
+    object_2 = execute_posts(query_2)
+    print_results(object_2)
